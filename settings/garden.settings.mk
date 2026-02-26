e816905afc56d2f9930345437f2956aad004b293
David Marshall <dmarshall@gmail.com>
2026-02-26 14:36:35 -0800

    new settings and templates

---
+++ settings/garden.settings.mk
@@ -0,0 +1,8 @@
+GENERIC = templates/stripes-side.yaml
+
+COLOR1 ?= \#9fe2bf
+COLOR2 ?= \#e8e4d8
+COLOR3 ?= \#f6d155
+COLOR4 ?= \#e8e4d8
+COLOR5 ?= \#f4c2c2
+COLOR6 ?= \#9fe2bf
---
+++ settings/warmearth.settings.mk
@@ -0,0 +1,8 @@
+GENERIC = templates/stripes-side.yaml
+
+COLOR1 ?= \#c9a227
+COLOR2 ?= \#ffd700
+COLOR3 ?= \#5c3310
+COLOR4 ?= \#e53935
+COLOR5 ?= \#c46a2d
+COLOR6 ?= \#808000
